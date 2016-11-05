3ebc9867fd17e270724da5350f61746796b028bc
submagr <agshubh191@gmail.com>
2016-11-06 01:40:36 +0530

    net correct loss incorrect

--- nms_convenet.py
+++ nms_convenet.py
@@ -7,9 +7,10 @@ from keras.utils.visualize_util import plot
 from keras.optimizers import Adam
 import theano
 import theano.tensor as T
+import numpy as np
 
-W = 100
-H = 100
+W = 1000
+H = 1000
 gridWidth = 4
 w = W/gridWidth
 h = H/gridWidth
@@ -21,16 +22,14 @@ scoreLayer.add(Convolution2D(512, 11, 11, border_mode='same', input_shape=(2, w,
 IoULayer = Sequential()
 IoULayer.add(Convolution2D(512, 1, 1, border_mode='same', input_shape=(121, w, h)))
 
-merged = Merge([scoreLayer, IoULayer], mode='concat')
+merged = Merge([scoreLayer, IoULayer], mode='concat', concat_axis = 1)
 
 final_model = Sequential()
 final_model.add(merged)
-final_model.add(Convolution2D(1024, 1, 1))
 final_model.add(Convolution2D(512, 1, 1))
 final_model.add(Convolution2D(512, 1, 1))
+final_model.add(Convolution2D(1, 1, 1))
 
-sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-adam_opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 
 def myloss(pred_val, true_val, count_pos, count_neg):
     loss = T.log(1.0+T.exp(-1.0*true_val*pred_val))
@@ -54,7 +53,21 @@ def test(y_true, y_pred):
     neg_loss_results, updates = theano.scan(fn = calcLossAtLocation, outputs_info = pos_loss_results[-1], sequences = [neg_indices[0], neg_indices[1]], non_sequences = [count_pos, count_neg, y_true, y_pred])
     return neg_loss_results[-1]
 
-final_model.compile(loss=test, optimizer=adam_opt)
+adam_opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+final_model.compile(loss='mean_squared_error', optimizer=adam_opt)
+print "Model compiled"
 
 plot(final_model, to_file='model.png', show_shapes=True)
-#final_model.fit(X_train, Y_train, batch_size=32, nb_epoch=1)
+score_train = np.random.random((10, 2, w, h))
+IoU_train = np.random.random((10, 121, w, h))
+y_train = np.random.randint(2, size = (10,1, w,h))
+y_train = y_train*2 - 1
+
+
+score_test = np.random.random((5, 2, w, h))
+IoU_test = np.random.random((5, 121, w, h))
+y_test = np.random.randint(2, size = (5,1,w,h))
+y_test = y_test*2 - 1
+print "Training started"
+final_model.fit([score_train, IoU_train], y_train, batch_size=1, nb_epoch=1)
+predictions =  final_model.test_on_batch([score_test, IoU_test], y_test)
